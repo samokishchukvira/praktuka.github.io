2c1aa3a01d338e3bf0fc303e82820726176a9cb7
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2024-12-06 23:36:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,6 +24,30 @@ if (window.innerWidth > 350) {
     });
   }
   
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30, 
+    centeredSlides: true, 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+    effect: "coverflow", 
+    coverflowEffect: {
+        rotate: 50, 
+        stretch: 0, 
+        depth: 100, 
+        modifier: 1, 
+        slideShadows: false,
+        },
+    });
+    
 const nav = document.querySelector(".nav"),
 searchIcon = document.querySelector("#searchIcon"),
 navOpenBtn = document.querySelector(".navOpenBtn"),
